33923ac46ebb6f91472be1a3eb894bf1fec47b7c
jhenao <J.Henao@cgiar.org>
2023-06-07 20:20:44 -0500

    Tic tac toe finished at logic level

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,34 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import confetti from "canvas-confetti"
+import Square from "./components/Square"
+import checkWinner from "./logic/board"
+import { TURNS } from "./constants"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ board, setBoard ]   = useState(Array(9).fill(null))
+  const [ turn, setTurn ]     = useState(TURNS.X)
+  const [ winner, setWinner ] = useState(null)
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null)
+  }
+    
+  const updateBoard = (index) => {
+    //Don't change the square if there's something or if there's a winner
+    if(board[index] || winner) return
+    // update the board
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    //Change the turn
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn) 
+    //check if there's a winner
+    const newWinner = checkWinner(newBoard) 
+    if(newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if(checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Reset</button>
+      <section className="game"> 
+        {
+          board.map((square, index) => {
+            return (
+              <Square 
+                key={index} 
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+      <section className="result">
+        { 
+          winner !== null && (
+            <section className="winner">
+              <div className="text">
+                <h3>
+                  {
+                    winner === false ? 'Draw' : 'Won'
+                  }
+                </h3>
+                <header className="win">
+                  { winner && <Square>{winner}</Square>}
+                </header>
+                <footer>
+                  <button onClick={resetGame}>Start again</button>
+                </footer>
+              </div>
+            </section>
+          ) 
+        }
+      </section>
+    </main>
   )
 }
 
---
+++ projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,18 @@
+
+import { WINNER_COMBOS } from "../constants"
+
+const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+       const [a, b, c] = combo
+       if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] 
+       ) {
+        return boardToCheck[a]
+       }
+    }
+    return null
+}
+
+export default checkWinner
